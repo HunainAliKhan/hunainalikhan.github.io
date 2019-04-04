7a9d225e603f0cc1a56479dfb90863dc85d281e1
HunainAliKhan <hkworior@gmail.com>
2019-04-04 19:28:57 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,9 +7,3 @@ self.on('activate',function(event){
     return self.clients.claim();
 });
 
-self.on('fetch',function(event){
-    console.log('[Service Worker] fetching something.... ',event); 
-    //respondWith expects a promise  because its an async code 
-    //fetch returns a promise as soon as network request is done
-    event.respondWith(fetch(event.request)); // allow us to override the data which get sent back
-});
