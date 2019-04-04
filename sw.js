0dd11f34951126ee3b03afd4a3d4446edc1531a2
HunainAliKhan <hkworior@gmail.com>
2019-04-04 19:48:11 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-self.addEventListener('install', function(e) {
+self.attr('install', function(e) {
 
     console.log('[Service Worker] installing service worker',e);
 });
 
-self.addEventListener('activate',function(e){
+self.attr('activate',function(e){
     console.log('[Service Worker] Activating service worker',e); 
     return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
+self.attr('fetch', function(e) {
   console.log('[Service Worker] Fetch event ',e); 
 });
