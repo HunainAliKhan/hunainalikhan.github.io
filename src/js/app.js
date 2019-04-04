5d0d34d8a6421e660d1b937332fa3fee4649b742
HunainAliKhan <hkworior@gmail.com>
2019-04-04 19:22:36 +0500

    changing path of service worker

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 
 if('serviceWorker' in navigator){
     navigator.serviceWorker
-    .register('https://hunainalikhan.github.io/sw.js')
+    .register('./sw.js')
     .then(function(){
         console.log('service worker register');
     }).catch(function(err){
